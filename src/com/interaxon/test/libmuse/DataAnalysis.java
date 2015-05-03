47faef981ae23345bf0740aa5cd5ff05d3cff505
Robert <rvanspyk@gmail.com>
2015-05-03 14:48:48 -0700

    DataBuffer now creates ArrayList that should contain the values, but not tested. TODO: get DataAnalysis to load data into models. Then need to have ability to store 5 normal + 5 stressed Instances for training.

--- src/com/interaxon/test/libmuse/DataAnalysis.java
+++ src/com/interaxon/test/libmuse/DataAnalysis.java
@@ -65,6 +65,33 @@ public class DataAnalysis
     // TODO: Function to convert data into Instance
     // TODO: Function to combine each sensor Instance into Instances
     public Instances aggregateInstances() {
+        // ArrayList<Double> to some element in fvWekaAttributes.
+        alpScorDB.dataSamples;
+
+        // Declare two numeric attributes
+        Attribute Attribute1 = new Attribute(“firstNumeric”);
+        Attribute Attribute2 = new Attribute(“secondNumeric”);
+
+        // Declare a nominal attribute along with its values
+        FastVector fvNominalVal = new FastVector(3);
+        fvNominalVal.addElement(“blue”);
+        fvNominalVal.addElement(“gray”);
+        fvNominalVal.addElement(“black”);
+        Attribute Attribute3 = new Attribute(“aNominal”, fvNominalVal);
+
+        // Declare the class attribute along with its values
+        FastVector fvClassVal = new FastVector(2);
+        fvClassVal.addElement(“positive”);
+        fvClassVal.addElement(“negative”);
+        Attribute ClassAttribute = new Attribute(“theClass”, fvClassVal);
+
+        // Declare the feature vector
+        FastVector fvWekaAttributes = new FastVector(4);
+        fvWekaAttributes.addElement(Attribute1);
+        fvWekaAttributes.addElement(Attribute2);
+        fvWekaAttributes.addElement(Attribute3);
+        fvWekaAttributes.addElement(ClassAttribute);
+
         // Create the instance
         Instance iExample = new DenseInstance(4);
         iExample.setValue((Attribute)fvWekaAttributes.elementAt(0), 1.0);
--- src/com/interaxon/test/libmuse/DataBuffer.java
+++ src/com/interaxon/test/libmuse/DataBuffer.java
@@ -2,6 +2,8 @@ package com.interaxon.test.libmuse;
 
 // Someone needs to make this code more portable!
 
+// ArrayList<Double> Packet.getValues();
+
 import java.util.concurrent.ConcurrentLinkedQueue;
 import com.interaxon.libmuse.MuseDataPacket;
 import com.interaxon.libmuse.MuseDataPacketType;
@@ -9,12 +11,13 @@ import com.interaxon.libmuse.MuseDataPacketType;
 public class DataBuffer
 {
     private final int SIZE_LIMIT = 2048;
-    public ConcurrentLinkedQueue<MuseDataPacket> incomingData;
+    public ConcurrentLinkedQueue<MuseDataPacket> incomingPackets;
     public int size;
+    public ArrayList<Double> dataSamples;
 
     public DataBuffer()
     {
-        this.incomingData = new ConcurrentLinkedQueue();
+        this.incomingPackets = new ConcurrentLinkedQueue();
         this.size = new Integer();
     }
 
@@ -24,16 +27,18 @@ public class DataBuffer
 
     public MuseDataPacket pop() {
         this.size--;
-        return (MuseDataPacket)this.incomingData.poll();
+        return (MuseDataPacket)this.incomingPackets.poll();
     }
 
     public void receive(MuseDataPacket p)
     {
-        getClass(); if (this.incomingData.size() > SIZE_LIMIT) {
+        getClass(); if (this.incomingPackets.size() > SIZE_LIMIT) {
         this.size--;
-        this.incomingData.poll();
+        this.incomingPackets.poll();
+        this.dataSamples.removeRange(0,16); // ASSUMES 16 values / packet
     }
-        this.incomingData.add(p);
+        this.incomingPackets.add(p);
+        this.dataSamples.addAll(p.getValues());
         this.size++;
     }
 }
